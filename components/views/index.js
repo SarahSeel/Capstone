333ce9b173ff57f2d41a9238355e8451b07dcbd0
SarahSeel <sarahizsilly09@yahoo.com>
2022-09-25 18:33:55 -0500

    Homework week 6

--- components/views/index.js
+++ components/views/index.js
@@ -1,4 +1,4 @@
-export { default as Destinations } from "./Destination";
+export { default as Destinations } from "./Destinations";
 export { default as Events } from "./Events";
 export { default as Home } from "./Home";
 export { default as Plantrip } from "./Plantrip";
