219930a415d7b4743b4c80fd350e327872ee4def
SarahSeel <sarahizsilly09@yahoo.com>
2022-09-25 09:32:18 -0500

    Capstone push 9-25

---
+++ components/views/Home.js
@@ -0,0 +1,13 @@
+import html from "html-literal";
+
+export default state => html`
+<div><main>
+  <p>This is the home page for my motorcycle application.
+    This will cover more info on how to navigate through this page.
+    Also will include some nice images and colors. It will be a work in progress.
+  </p>
+</div>
+</main>
+<div>
+    <img class="img" src="https://images.pexels.com/photos/5921940/pexels-photo-5921940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+</div>`;
---
+++ store/Links.js
@@ -0,0 +1,5 @@
+export default [
+    { title: "Destinations", text: "Destinations" },
+    { title: "Events", text: "Local Events" },
+    { title: "Plan trip", text: "Lets Plan a Trip" }
+];
