13b7f6a1c5724872b762e27a4d711db1f711d4f0
SarahSeel <sarahizsilly09@yahoo.com>
2022-10-13 19:01:19 -0500

    Homework Week 9

--- components/views/Map.js
+++ components/views/Map.js
@@ -1,3 +1,9 @@
 import html from "html-literal";
 
-export default state => html``;
+export default state => html`
+  <!-- <script src="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.js"></script>
+  <link
+    href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css"
+    rel="stylesheet"
+  /> -->
+`;
