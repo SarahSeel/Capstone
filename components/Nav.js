ffd358ad30791a2f409fc2e924396e401917eeda
SarahSeel <sarahizsilly09@yahoo.com>
2022-10-26 10:51:03 -0500

    Updated Map Css

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,13 @@ export default links => html`
         ${links
           .map(
             link =>
-              `<li><a href="/${link.title}" title="${link.title}" data-navigo <i class="${link.icon}" </i> ${link.text}</a></li>`
+              html`
+                <li>
+                  <a href="/${link.title}" title="${link.title}" data-navigo>
+                    <i class="${link.icon}"></i> ${link.text}</a
+                  >
+                </li>
+              `
           )
           .join("")}
       </ul>
